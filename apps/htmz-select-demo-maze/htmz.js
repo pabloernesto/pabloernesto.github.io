71d62967bf87b9499c6e0464d329db229db0e313
Pablo Ernesto Repetto <pablo.ernesto.repetto@gmail.com>
2026-02-23 18:48:29 -0300

    htmz-select: removed unused check

--- apps/htmz-select-demo-maze/htmz.js
+++ apps/htmz-select-demo-maze/htmz.js
@@ -7,11 +7,10 @@ function htmz(frame) {
   const fragments = decodeURIComponent(frame.contentWindow.location.hash).split(" ");
   frame.contentWindow.location.hash = fragments[0];
   const q = fragments.slice(1).join(" "); // `li, .listitem` is valid
-
   if (q) {
-  frame.contentDocument.body.replaceChildren(
-    ...frame.contentDocument.querySelectorAll(q || null)
-  );
+    frame.contentDocument.body.replaceChildren(
+      ...frame.contentDocument.querySelectorAll(q)
+    );
   }
   // select end
 
